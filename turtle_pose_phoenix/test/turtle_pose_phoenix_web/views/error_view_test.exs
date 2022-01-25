5b167ccb7dd1d2bedc7da3d5044917ea580d7f7d
NISHIUCHI Kazuma <nishiuchi.kazuma@gmail.com>
2022-01-25 09:51:24 +0900

    run mix format

--- turtle_pose_phoenix/test/turtle_pose_phoenix_web/views/error_view_test.exs
+++ turtle_pose_phoenix/test/turtle_pose_phoenix_web/views/error_view_test.exs
@@ -9,6 +9,7 @@ defmodule TurtlePosePhoenixWeb.ErrorViewTest do
   end
 
   test "renders 500.html" do
-    assert render_to_string(TurtlePosePhoenixWeb.ErrorView, "500.html", []) == "Internal Server Error"
+    assert render_to_string(TurtlePosePhoenixWeb.ErrorView, "500.html", []) ==
+             "Internal Server Error"
   end
 end
