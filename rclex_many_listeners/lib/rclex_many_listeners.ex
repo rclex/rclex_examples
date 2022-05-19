8bf24ea14df48724c6e80f67ec885f3e00051411
s-hosoai <shintaro.hosoai@change-vision.com>
2022-05-19 16:23:45 +0900

    Impl many listeners

--- rclex_many_listeners/lib/rclex_many_listeners.ex
+++ rclex_many_listeners/lib/rclex_many_listeners.ex
@@ -1,18 +1,22 @@
 defmodule RclexManyListeners do
-  @moduledoc """
-  Documentation for `RclexManyListeners`.
-  """
+  def subscribe_message(num_listeners) do
+    context = Rclex.rclexinit()
+    {:ok, nodes} = Rclex.ResourceServer.create_nodes(context, 'listener', num_listeners)
+    {:ok, subscribers} = Rclex.Node.create_subscribers(nodes, 'StdMsgs.Msg.String', 'chatter', :single)
+    Rclex.Subscriber.start_subscribing(subscribers, context, &sub_callback/1)
 
-  @doc """
-  Hello world.
+    Process.sleep(10000)
 
-  ## Examples
+    Rclex.Subscriber.stop_subscribing(subscribers)
+    Rclex.Node.finish_jobs(subscribers)
+    Rclex.ResourceServer.finish_nodes(nodes)
+    Rclex.shutdown(context)
+  end
 
-      iex> RclexManyListeners.hello()
-      :world
+  defp sub_callback(msg) do
+    recv_msg = Rclex.Msg.read(msg, 'StdMsgs.Msg.String')
+    msg_data = List.to_string(recv_msg.data)
 
-  """
-  def hello do
-    :world
+    IO.puts("Rclex: received msg: #{msg_data}")
   end
 end
--- rclex_many_listeners/test/rclex_many_listeners_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule RclexManyListenersTest do
-  use ExUnit.Case
-  doctest RclexManyListeners
-
-  test "greets the world" do
-    assert RclexManyListeners.hello() == :world
-  end
-end
