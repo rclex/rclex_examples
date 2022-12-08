d50db07ae2f3316652116dad45e89cd1159737e0
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2022-12-08 16:00:46 +0900

    mix new rclex_topics

---
+++ rclex_topics/lib/rclex_topics.ex
@@ -0,0 +1,18 @@
+defmodule RclexTopics do
+  @moduledoc """
+  Documentation for `RclexTopics`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RclexTopics.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
